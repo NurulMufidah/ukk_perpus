d8e9d5844c48273b890d2bd0fa306a4d6946740e
NurulMufidah <bcaa44279@gmail.com>
2025-01-10 14:50:31 +0700

    commit pertama

---
+++ database/migrations/2025_01_10_073435_create_bukus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->foreignId('kategori_id') Constrained('kategori_buku');
+            $table->string('penulis');
+            $table->string('penerbit');
+            $table->int('tahun_penerbit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bukus');
+    }
+};
